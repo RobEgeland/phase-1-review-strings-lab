c36c15f376a020fc45b3cf7c1e66cda4537048fb
Robert Egeland <robegeland@yahoo.com>
2021-12-14 19:58:57 -0600

    commit tree a2786ec46a1763d188c77cd4362012906661fc47

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'Rob Egeland';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
